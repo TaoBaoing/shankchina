2ddf2d2b2451916fbe5b8a968832e5ee0d39adfd
hui luan <luanzhiyong20062@163.com>
2016-03-26 22:10:58 +0800

    .

---
+++ 爬虫/爬虫/Dfcf.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace 爬虫
+{
+   public  class Dfcf
+    {
+        //http://data.eastmoney.com/DataCenter_V3/gdzjc.ashx?pagesize=50&page=3&js=var%20diSTAHjb&param=&sortRule=-1&sortType=BDJZ&tabid=jzc&code=&name=
+
+       public void StartParser()
+       {
+           for (int i = 0; i < 10; i++)
+           {
+               var url = "http://data.eastmoney.com/DataCenter_V3/gdzjc.ashx?pagesize=50&page=" + i +"&js=var%20diSTAHjb&param=&sortRule=-1&sortType=BDJZ&tabid=jzc&code=&name=";
+
+
+           }
+       }
+    }
+}
---
+++ 爬虫/爬虫/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 爬虫
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //http://data.eastmoney.com/DataCenter_V3/gdzjc.ashx?pagesize=50&page=3&js=var%20diSTAHjb&param=&sortRule=-1&sortType=BDJZ&tabid=jzc&code=&name=&rt=48633334
+
+            //http://data.eastmoney.com/DataCenter_V3/gdzjc.ashx?pagesize=50&page=2&js=var%20rYXLAFav&param=&sortRule=-1&sortType=BDJZ&tabid=jzc&code=&name=&rt=48633327
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
